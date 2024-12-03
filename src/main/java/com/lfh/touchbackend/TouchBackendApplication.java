0c0df621818137ba58ac4766eeea9f2ed999393a
lifenghe-code <li_fenghe@outlook.com>
2024-12-03 19:06:42 +0800

    init

---
+++ src/main/java/com/lfh/touchbackend/TouchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lfh.touchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouchBackendApplication.class, args);
+    }
+
+}
