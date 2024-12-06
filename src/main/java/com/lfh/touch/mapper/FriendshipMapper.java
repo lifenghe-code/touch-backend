03f8cb6f8fb96c5ed70f0e53a2a6be5cac44983e
lifenghe-code <li_fenghe@outlook.com>
2024-12-06 19:12:55 +0800

    mapper文件

---
+++ src/main/java/com/lfh/touch/mapper/FriendshipMapper.java
@@ -0,0 +1,18 @@
+package com.lfh.touch.mapper;
+
+import com.lfh.touch.model.domain.Friendship;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author li_fe
+* @description 针对表【friendship(好友申请表)】的数据库操作Mapper
+* @createDate 2024-12-04 18:43:50
+* @Entity com.lfh.touch.model.domain.Friendship
+*/
+public interface FriendshipMapper extends BaseMapper<Friendship> {
+
+}
+
+
+
+
