b3491e923da1b146f59306eecb07b247f3ffe024
lifenghe-code <li_fenghe@outlook.com>
2024-12-06 15:06:18 +0800

    用户实体类

---
+++ src/main/java/com/lfh/touch/model/domain/User.java
@@ -0,0 +1,85 @@
+package com.lfh.touch.model.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 用户表
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User implements Serializable {
+    /**
+     * 用户ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer uid;
+
+    /**
+     * 用户账号
+     */
+    private String account;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    /**
+     * 用户昵称
+     */
+    private String nickname;
+
+    /**
+     * 用户头像url
+     */
+    private String avatar;
+
+    /**
+     * 主页背景图url
+     */
+    private String background;
+
+    /**
+     * 性别 0女 1男 2未知
+     */
+    private Integer gender;
+
+    /**
+     * 个性签名
+     */
+    private String description;
+
+    /**
+     * 状态 0正常 1封禁 2注销
+     */
+    private Integer status;
+
+    /**
+     * 角色类型 0普通用户 1管理员 2超级管理员
+     */
+    private Integer role;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
