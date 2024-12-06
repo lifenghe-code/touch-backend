e5c98594d4909b2a1c53e7e0e6cf4d2356766331
lifenghe-code <li_fenghe@outlook.com>
2024-12-06 14:58:32 +0800

    mapper文件

---
+++ src/main/java/com/lfh/touch/mapper/ChatMapper.java
@@ -0,0 +1,18 @@
+package com.lfh.touch.mapper;
+
+import com.lfh.touch.model.domain.Chat;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author li_fe
+* @description 针对表【chat(聊天表)】的数据库操作Mapper
+* @createDate 2024-12-04 18:40:15
+* @Entity com.lfh.touch.model.domain.Chat
+*/
+public interface ChatMapper extends BaseMapper<Chat> {
+
+}
+
+
+
+
