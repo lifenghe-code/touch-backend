0663dcdd6eec860c17478ae81b90714a075d48e1
lifenghe-code <li_fenghe@outlook.com>
2024-12-06 14:54:10 +0800

    mapper类

---
+++ src/main/java/com/lfh/touch/mapper/ChatDetailMapper.java
@@ -0,0 +1,18 @@
+package com.lfh.touch.mapper;
+
+import com.lfh.touch.model.domain.ChatDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author li_fe
+* @description 针对表【chat_detail(聊天记录表)】的数据库操作Mapper
+* @createDate 2024-12-04 18:40:17
+* @Entity com.lfh.touch.model.domain.ChatDetail
+*/
+public interface ChatDetailMapper extends BaseMapper<ChatDetail> {
+
+}
+
+
+
+
---
+++ src/main/java/com/lfh/touch/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.lfh.touch.mapper;
+
+import com.lfh.touch.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author li_fe
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2024-12-04 18:40:38
+* @Entity com.lfh.touch.model.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
