65ee6279cce06e41466a1f929e1398fb0213a1c9
lifenghe-code <li_fenghe@outlook.com>
2024-12-14 11:02:57 +0800

    聊天信息请求类

---
+++ src/main/java/com/lfh/touch/model/dto/chat/ChatGetDetailRequest.java
@@ -0,0 +1,10 @@
+package com.lfh.touch.model.dto.chat;
+
+import java.io.Serializable;
+
+public class ChatGetDetailRequest implements Serializable {
+
+
+
+    private static final long serialVersionUID = 1L;
+}
