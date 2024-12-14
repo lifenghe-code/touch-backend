33b4d33547002a3def581f844e3b59dd9a438a29
lifenghe-code <li_fenghe@outlook.com>
2024-12-14 11:04:51 +0800

    用户请求更新本人信息类

--- src/main/java/com/lfh/touch/model/dto/user/UserUpdateMyRequest.java
+++ src/main/java/com/lfh/touch/model/dto/user/UserUpdateMyRequest.java
@@ -2,8 +2,10 @@ package com.lfh.touch.model.dto.user;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UserUpdateMyRequest {
+public class UserUpdateMyRequest implements Serializable {
     /**
      * id
      */
