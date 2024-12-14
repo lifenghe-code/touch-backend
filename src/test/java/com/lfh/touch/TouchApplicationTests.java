566bcde2afc27c86d12bd443ee8c5e83c15acbc1
lifenghe-code <li_fenghe@outlook.com>
2024-12-14 11:09:44 +0800

    测试

--- src/test/java/com/lfh/touch/TouchApplicationTests.java
+++ src/test/java/com/lfh/touch/TouchApplicationTests.java
@@ -1,7 +1,5 @@
 package com.lfh.touch;
 
-import com.lfh.touch.netty.NettyClient;
-import com.lfh.touch.netty.NettyServer;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,26 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TouchApplicationTests {
 
-    @Test
-    public static void main(String[] args) {
-        // 启动服务器
-        new Thread(() -> {
-            NettyServer server = new NettyServer();
-            try {
-                server.start();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
-
-        // 启动客户端
-
-        Integer userId = 1;
-        NettyClient client = new NettyClient(userId);
-        client.start();
-
-        // 发送好友请求示例
-        client.sendAddFriendRequest(1, "我是user1，请添加我为好友");
-    }
 
 }
