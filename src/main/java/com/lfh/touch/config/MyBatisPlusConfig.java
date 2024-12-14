761feb645de6d2e5546f187f88655c368849e551
lifenghe-code <li_fenghe@outlook.com>
2024-12-14 11:00:30 +0800

    MyBatis-plus配置类

---
+++ src/main/java/com/lfh/touch/config/MyBatisPlusConfig.java
@@ -0,0 +1,39 @@
+package com.lfh.touch.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.lfh.touch.mapper.ChatDetailMapper;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * MyBatis Plus 配置
+ *
+ * @author lfh
+ */
+@Configuration
+@MapperScan("com.lfh.touch.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * 拦截器配置
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
