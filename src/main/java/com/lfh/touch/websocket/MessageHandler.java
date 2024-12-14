aa945b6cff0bdc13b6fec5f6266e08e28a195bf7
lifenghe-code <li_fenghe@outlook.com>
2024-12-14 11:04:22 +0800

    websocket请求处理类

--- src/main/java/com/lfh/touch/websocket/MessageHandler.java
+++ src/main/java/com/lfh/touch/websocket/MessageHandler.java
@@ -1,25 +1,34 @@
 package com.lfh.touch.websocket;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.map.MapUtil;
 import cn.hutool.json.JSONUtil;
 import com.lfh.touch.constant.MessageConstant;
 import com.lfh.touch.mapper.ChatDetailMapper;
+import com.lfh.touch.mapper.UserMapper;
 import com.lfh.touch.model.domain.ChatDetail;
+import com.lfh.touch.model.domain.User;
 import com.lfh.touch.model.dto.user.UserAddFriendRequest;
 import com.lfh.touch.utils.SpringContextUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.websocket.Session;
+import java.util.Map;
+
 @Slf4j
 public class MessageHandler{
     public static void handleFriendAddRequest(Session toSession, UserAddFriendRequest userAddFriendRequest){
-        String jsonString = JSONUtil.toJsonStr(userAddFriendRequest);
+        UserMapper userMapper = SpringContextUtils.getBean(UserMapper.class);
+        User user = userMapper.selectById(userAddFriendRequest.getSenderUid());
+        String jsonString = JSONUtil.toJsonStr(user);
         toSession.getAsyncRemote().sendText(jsonString);
     }
 
-    public static void sendToOne(Session fromSession, Session toSession, ChatDetail chatDetail) {
+    public static void sendToOne(String type, Session fromSession, Session toSession, ChatDetail chatDetail) {
         // 通过sid查询map中是否存在
         Integer toUid = chatDetail.getReceiverUid();
         String message = chatDetail.getContent();
+
         if (toSession == null) {
             log.error("服务端给客户端发送消息 ==> toSid = {} 不存在, message = {}", toUid, message);
             return;
@@ -30,7 +39,10 @@ public class MessageHandler{
         int insert = chatDetailMapper.insert(chatDetail);
         chatDetail.setId(insert);
         log.info("服务端给客户端发送消息 ==> toSid = {}, message = {}", toUid, message);
-        String jsonString = JSONUtil.toJsonStr(chatDetail);
+        Map<String, Object> stringObjectMap = BeanUtil.beanToMap(chatDetail);
+        stringObjectMap.put("type",type);
+        String jsonString = JSONUtil.toJsonStr(stringObjectMap);
+
         toSession.getAsyncRemote().sendText(jsonString);
         fromSession.getAsyncRemote().sendText(jsonString);
     }
