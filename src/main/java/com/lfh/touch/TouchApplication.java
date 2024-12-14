15d634eb73cdb499d04eb61c82543afad8bd8be6
lifenghe-code <li_fenghe@outlook.com>
2024-12-14 11:05:27 +0800

    启动类

--- src/main/java/com/lfh/touch/TouchApplication.java
+++ src/main/java/com/lfh/touch/TouchApplication.java
@@ -1,24 +1,29 @@
 package com.lfh.touch;
 
-import com.lfh.touch.netty.NettyServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.lfh.touch.mapper")
 @ServletComponentScan // 扫描过滤器
+
 public class TouchApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(TouchApplication.class, args);
         // 开启
-        new Thread(() -> {
-            NettyServer server = new NettyServer();
-            server.start();
-        }).start();
+//        new Thread(() -> {
+//            NettyServer server = new NettyServer();
+//            try {
+//                server.start();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }).start();
     }
 
 }
